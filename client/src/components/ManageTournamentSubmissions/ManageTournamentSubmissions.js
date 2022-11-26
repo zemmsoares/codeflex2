aed63cbf937d2c5a2ad17395dbcc028731b5ebd8
zemmsoares <zemmsoares@gmail.com>
2022-11-26 18:05:26 +0000

    add download button for tournament subs

--- client/src/components/ManageTournamentSubmissions/ManageTournamentSubmissions.js
+++ client/src/components/ManageTournamentSubmissions/ManageTournamentSubmissions.js
@@ -68,8 +68,29 @@ function ManageTournamentSubmissions() {
     for (let i = 0; i < filterSubmissions.length; i++) {}
   }
 
+  function plagCheck() {
+    var dataStr =
+      "data:text/json;charset=utf-8," +
+      encodeURIComponent(JSON.stringify(filteredUsers));
+    var downloadAnchorNode = document.createElement("a");
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute(
+      "download",
+      "tournament-" + tournamentId + "-submissions" + ".json"
+    );
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
+
   return (
     <div>
+      <input
+        type="submit"
+        className="mt-8 ml-8 py-1.5 px-5 mr-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        value="Download Submissions File!"
+        onClick={plagCheck}
+      />
       {filteredData ? (
         <div>
           {Object.keys(filteredUsers).map((key, index) => {
